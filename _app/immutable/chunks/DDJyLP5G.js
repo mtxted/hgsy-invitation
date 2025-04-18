66ddd0e181a800a6925d25972b65b431834ec3cb
mtxted <mtxted@naver.com>
2025-04-18 18:20:45 +0900

    Updates

---
+++ _app/immutable/chunks/DDJyLP5G.js
@@ -0,0 +1,10 @@
+const t={title:"변현규 ♥ 박서윤의 결혼식에 초대합니다."},e={date:"2026년 4월 25일(토) 오후 1시 40분",place:"서울 강동구 천호대로 1102 KDW웨딩 3층 마이더스 홀"},n={date:"2026.4.25",sub_title:"두 사람의 결혼식에 초대합니다.",letter_content:`바람이 불고, 햇살이 스며든 어느 날
+서로의 이름을 조심스레 불러보던 우리가
+이제 같은 이름 아래, 같은 꿈을 꾸려 합니다.
+
+마음과 마음이 닿아
+계절처럼 자연스럽게 하나가 된 이 날
+사랑하는 이들과 그 기쁨을 나누고 싶습니다.
+
+오셔서 저희의 시작에
+따뜻한 꽃잎 하나, 빛 한 줄기 되어 주세요.`},l={title:"사진 모음",sub_title:"사진을 탭하면 더 크게 볼 수 있습니다."},o={title:"예식장",address_copied:"주소를 복사했습니다."},c={meta:t,cover:e,letter:n,gallery:l,location:o};export{e as cover,c as default,l as gallery,n as letter,o as location,t as meta};
